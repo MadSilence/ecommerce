fbb0044df04d68b79f784e0e3edec20078091384
manneanil99 <55541402+manneanil99@users.noreply.github.com>
2023-03-15 14:13:06 +0530

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {LoginComponent} from './login/login.component';
-
 
 @NgModule({
   declarations: [
@@ -22,14 +16,12 @@ import {LoginComponent} from './login/login.component';
     FooterComponent,
     LoginComponent,
     
-   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     SlickCarouselModule,
-    
     RouterModule,
     
   ],
@@ -37,23 +29,3 @@ import {LoginComponent} from './login/login.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-=======
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
->>>>>>> 9210c5028be3b47dba538741305d1ab5477ea716
